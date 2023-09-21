b9e818dd70b1535bacd727794a63781d5110493a
unknown <paulopraddo011@gmail.com>
2023-09-21 17:55:32 -0300

    trabalhando na segurança da aplicação

--- src/main/java/com/paulo/praddo/projetorestaurante/model/User.java
+++ src/main/java/com/paulo/praddo/projetorestaurante/model/User.java
@@ -1,9 +1,6 @@
 package com.paulo.praddo.projetorestaurante.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,6 +18,8 @@ import java.util.List;
 @Getter
 public class User implements UserDetails {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String login;
