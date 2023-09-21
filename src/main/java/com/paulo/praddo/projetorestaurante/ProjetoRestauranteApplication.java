cd096ec688eef382a4ca9084bc931a6572d6873a
unknown <paulopraddo011@gmail.com>
2023-09-21 13:33:26 -0300

    first commit

---
+++ src/main/java/com/paulo/praddo/projetorestaurante/ProjetoRestauranteApplication.java
@@ -0,0 +1,13 @@
+package com.paulo.praddo.projetorestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paulo/praddo/projetorestaurante/ProjetoRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paulo.praddo.projetorestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
